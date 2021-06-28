3b723edcf834388a73a241d13417f6905a6c486d
Himesha <himeshmahee99@gmail.com>
2021-06-29 01:33:09 +0530

    firat commit

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-10 mx-auto text-title">
+                    <h1>your cart empty</h1>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+import { ProductProvider } from './Context';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>
+  ,
   document.getElementById('root')
 );
 
